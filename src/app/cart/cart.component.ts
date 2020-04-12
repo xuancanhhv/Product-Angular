16deef6239f5b557d156a8ee3c73ec00fbb381fd
xuancanhhv <comprocoding@gmail.com>
2020-04-12 22:06:41 +0700

    2 files modified, 1 added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
 import { FormBuilder } from '@angular/forms';
+import {Hero} from '../hero';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -17,7 +18,7 @@ checkoutForm;
       address: ''
     });
               }
-
+  submitted = false;
   ngOnInit() {
    this.items = this.cartService.getItems();
   }
@@ -25,8 +26,12 @@ checkoutForm;
     // Process checkout data here
     this.items = this.cartService.clearCart();
     this.checkoutForm.reset();
-
+    this.submitted = true;
     console.warn('Your order has been submitted', customerData);
   }
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
 
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  get diagnostic() { return JSON.stringify(this.model); }
 }
